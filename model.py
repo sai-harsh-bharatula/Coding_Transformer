1d4635c3a69babf1c979f8f4b5d995af01590e96
Sai Harsh Bharatula <b.saiharsh@gmail.com>
2024-12-02 17:19:46 -0500

    Decober

--- model.py
+++ model.py
@@ -150,4 +150,14 @@ class DecoderBlock(nn.Module):
         x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
         x = self.residual_connections[2](x, self.feed_forward_block)
         return x 
-        
+
+class Decoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList)-> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
